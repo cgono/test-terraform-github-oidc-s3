5a6a0ac37ad58cb1132e8f244499fc56a4dc75fe
Clint Mark Gono <clint@clintgono.com>
2025-06-22 16:55:46 +0800

    Attempt: polling

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -6,9 +6,21 @@ resource "aws_iam_policy" "s3_upload_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ],
         Resource = ["${var.bucket_arn}", "${var.bucket_arn}/*"]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "guardduty:ListDetectors",
+          "guardduty:ListFindings"
+        ],
+        Resource = "*"
       }
     ]
   })
